dadc1e579d06eb231ea46cc786d066226aafd7cf
Dario <dariojahic95@gmail.com>
2016-04-28 19:48:39 +0200

    Napravaljen osnovni prozor.

--- MenjacnicaGUI/src/menjacnica/gui/MenjacnicaGUI.java
+++ MenjacnicaGUI/src/menjacnica/gui/MenjacnicaGUI.java
@@ -6,10 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.Dimension;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JButton;
+import java.awt.Toolkit;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 
 public class MenjacnicaGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -31,12 +42,47 @@ public class MenjacnicaGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public MenjacnicaGUI() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(MenjacnicaGUI.class.getResource("/resources/slika1.jpg")));
+		setTitle("Menjacnica");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("FIle");
+		menuBar.add(mnFile);
+		
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel eastPanel = new JPanel();
+		eastPanel.setPreferredSize(new Dimension(100, 10));
+		contentPane.add(eastPanel, BorderLayout.EAST);
+		
+		JButton btnDodajKurs = new JButton("Dodaj kurs");
+		eastPanel.add(btnDodajKurs);
+		
+		JButton btnIzbrisiKurs = new JButton("Izbrisi kurs");
+		eastPanel.add(btnIzbrisiKurs);
+		
+		JButton btnIzvrsiZamenu = new JButton("Izvrsi zamenu");
+		eastPanel.add(btnIzvrsiZamenu);
+		
+		JPanel southPanel = new JPanel();
+		southPanel.setPreferredSize(new Dimension(10, 60));
+		southPanel.setBorder(new TitledBorder(null, "Status", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.add(southPanel, BorderLayout.SOUTH);
+		
+		JPanel centerPanel = new JPanel();
+		contentPane.add(centerPanel, BorderLayout.CENTER);
+		
+		table = new JTable();
+		centerPanel.add(table);
 	}
 
 }
